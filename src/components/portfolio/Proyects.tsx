20ac30206781c31d0a43144e07708ff83465f808
eXVite <129987275+eXVite@users.noreply.github.com>
2025-03-04 21:40:38 +0100

    imagenes y grid provisional demos

--- src/components/portfolio/Proyects.tsx
+++ src/components/portfolio/Proyects.tsx
@@ -6,7 +6,7 @@ const Container = styled.div`
   display: flex;
   flex-direction: column;
   gap: 3rem;
-  padding: 2rem 4rem;
+  padding: 2rem 0 0 0;
 `;
 
 const TitleContainer = styled.div`
@@ -21,11 +21,11 @@ const DescContainer = styled.div`
 const TitleDescContainer = styled.div`
   display: flex;
   gap: 3rem;
+  padding: 0 7rem;
 `;
 
 const ProyectsContainer = styled.div`
   width: 100%;
-  height: 300px;
   background-image: linear-gradient(
       0deg,
       transparent 95%,
@@ -38,6 +38,54 @@ const ProyectsContainer = styled.div`
     );
   background-size: 25px 25px;
   background-position: -8.5px -8.5px; /* Ajusta la posición para eliminar bordes */
+`;
+
+const ProyectsGrid = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 2fr;
+  grid-template-columns: 1fr 1fr;
+  height: 100%;
+  box-sizing: border-box;
+`;
+
+const BigProyectItem = styled.div`
+  grid-column-start: 2;
+  grid-column-end: 2;
+  grid-row-start: 1;
+  grid-row-end: 3;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding-right: 7rem;
+`;
+
+const SmallProyectItem = styled.div`
+  grid-column-start: 1;
+  grid-column-end: 1;
+  grid-row-start: 1;
+  grid-row-end: 1;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 4rem 4rem 0 7rem;
+  padding-left: 7rem;
+`;
+
+const SmallProyectItem2 = styled.div`
+  grid-column-start: 1;
+  grid-column-end: 1;
+  grid-row-start: 2;
+  grid-row-end: 2;
+  display: flex;
+  justify-content: left;
+  align-items: center;
+  padding: 4rem 0 4rem 7rem;
+  align-items: center;
+`;
+
+const Image = styled.img<{ height: string }>`
+  height: ${(props) => props.height};
+  border-radius: 1rem;
 `;
 
 const Proyects = () => {
@@ -64,7 +112,21 @@ const Proyects = () => {
           />
         </DescContainer>
       </TitleDescContainer>
-      <ProyectsContainer></ProyectsContainer>
+      <ProyectsContainer>
+        <ProyectsGrid>
+          <SmallProyectItem>
+            <Image src="/images/proyects/dashboard.webp" height={"15rem"} />
+          </SmallProyectItem>
+          <SmallProyectItem2>
+            {" "}
+            <Image src="/images/proyects/landing.webp" height={"12rem"} />
+          </SmallProyectItem2>
+          <BigProyectItem>
+            {" "}
+            <Image src="/images/proyects/pizarra.webp" height={"15rem"} />
+          </BigProyectItem>
+        </ProyectsGrid>
+      </ProyectsContainer>
     </Container>
   );
 };
