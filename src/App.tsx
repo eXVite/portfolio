56c67e0347ae0de8d7f46dbf43f07c8bb8966849
eXVite <129987275+eXVite@users.noreply.github.com>
2025-04-07 20:15:33 +0200

    url management

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,19 @@
-// import { useEffect } from "react";
+import { useEffect } from "react";
 import { Outlet } from "react-router-dom";
-// import { useLocation } from "react-router-dom";
-// import { routes } from "./constants/Routes";
-// import { useNavigate } from "react-router-dom";
+import { useLocation } from "react-router-dom";
+import { routes } from "./constants/Routes";
+import { useNavigate } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
 import theme from "./themes";
 import { LanguageProvider } from "./contexts/TranslationContext";
 
 function App() {
-  // const location = useLocation();
-  // const navigate = useNavigate();
+  const location = useLocation();
+  const navigate = useNavigate();
 
-  // useEffect(() => {
-  //   if (location.pathname !== routes.base || location.pathname !== routes.demos)
-  //     navigate(routes.base);
-  // });
+  useEffect(() => {
+    if (location.pathname !== routes.base) navigate(routes.base);
+  });
 
   return (
     <LanguageProvider>
